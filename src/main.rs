448525c255a1d13ee3a987c4dd530a0b1558c461
muffin <diegoxe7117@gmail.com>
2022-02-22 11:56:55 +1300

    fmt

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     println!("{:?}", args);
 
-    // Open file from first arg    
-    let mut f = std::fs::File::open(args[1].to_string()).expect("File not found.");    
-    
+    // Open file from first arg
+    let mut f = std::fs::File::open(args[1].to_string()).expect("File not found.");
+
     // Try to open as bmp
     let img: bmp::Image = bmp::from_reader(&mut f).unwrap();
-    
+
     println!(
         "Read a BMP with dimensions {}x{}px",
         img.get_width(),
