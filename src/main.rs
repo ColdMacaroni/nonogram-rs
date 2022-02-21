f591d3e4248a9ca4c6eab88d8057b9486220d881
muffin <diegoxe7117@gmail.com>
2022-02-22 00:08:29 +1300

    Sample reading of a bmp file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate bmp;
+use bmp::Image;
+
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    println!("{:?}", args);
+
+    let mut f = std::fs::File::open(args[1].to_string()).unwrap();
+
+    let f = bmp::from_reader(&mut f).unwrap();
+    println!(
+        "Read a BMP with dimensions {}x{}px",
+        f.get_width(),
+        f.get_height()
+    );
+
     todo!();
 }
